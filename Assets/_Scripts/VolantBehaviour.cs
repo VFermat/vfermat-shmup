2ac069c5aaae87fafcf95893584cb2c5a0d247e5
VFermat <vitor.eller30@gmail.com>
2021-03-09 14:33:13 -0300

    Tutorial 1 e 2

---
+++ Assets/_Scripts/VolantBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolantBehaviour : SteerableBehaviour, IDamageable
+{
+    float angle = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        Die();
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    void FixedUpdate() {
+        angle += 0.1f;
+        if (angle > 2.0f * Mathf.PI) angle = 0.0f;
+        Thrust(0, Mathf.Cos(angle));
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.CompareTag("Shoot")) {
+            TakeDamage();
+        }
+    }
+}
