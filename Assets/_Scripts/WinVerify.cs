8161842e7f8f50b7af94890ee293b75f7ed6f60f
VFermat <vitor.eller30@gmail.com>
2021-03-15 23:40:25 -0300

    win verify

---
+++ Assets/_Scripts/WinVerify.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinVerify : MonoBehaviour
+{
+    private GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.GetInstance();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Enemy");
+        if (objs.Length <= 0) {
+            gm.ChangeState(GameManager.GameState.ENDGAME);
+            SceneManager.LoadScene("GameoverScene", LoadSceneMode.Single);
+        }
+    }
+}
